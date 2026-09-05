7dac889ac424a5771725140aad109480cb9c12f7
FREX007 <hailufirew0@gmail.com>
2026-09-05 23:01:36 +0300

    update

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,12 @@
 // routes/authRoutes.js
 import express from 'express';
-import { uploadFields } from '../utils/upload.js';
+import multer from 'multer';
 import { registerUser, loginUser } from '../controllers/authController.js';
+
+const upload = multer({ storage: multer.memoryStorage() });
 const router = express.Router();
 
-router.post('/register', uploadFields.single('businessLicenseFile'), registerUser);
+router.post('/register', upload.single('businessLicenseFile'), registerUser);
 router.post('/login', loginUser);
 
 export default router;
