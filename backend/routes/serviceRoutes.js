fd66d426a0462516f09eeb578cf7525b2bac6ca4
FREX007 <hailufirew0@gmail.com>
2026-09-05 10:44:35 +0300

    Update serviceRoutes multer storage for serverless

--- backend/routes/serviceRoutes.js
+++ backend/routes/serviceRoutes.js
@@ -1,12 +1,11 @@
-
 import express from 'express';
 import multer from 'multer';
-import { getApprovedServices, createService, requestServiceUpdate } from '../controllers/serviceController.js';
+import { getApprovedServices, createService, requestServiceUpdate, getMyServices, getServiceById } from '../controllers/serviceController.js';
 import { protect } from '../middleware/authMiddleware.js';
 import { uploadFields } from '../utils/upload.js';
-import { getMyServices, getServiceById } from '../controllers/serviceController.js';
 
-const upload = multer({ dest: 'uploads/'});
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 const router = express.Router();
 
 router.get('/', getApprovedServices); 
@@ -14,6 +13,5 @@ router.get('/my-services', protect, getMyServices);
 router.get('/:id', getServiceById);
 router.post('/', protect, uploadFields.single('certificationFile'), createService);
 router.put('/:id/request-update', protect, uploadFields.single('certificationFile'), requestServiceUpdate);
-
 
 export default router;
