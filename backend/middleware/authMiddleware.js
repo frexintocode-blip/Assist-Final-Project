baddd150b27899265ed022ce02e291fac45ee498
FREX007 <hailufirew0@gmail.com>
2026-09-11 18:02:22 +0300

    Update authMiddleware to read JWT from httpOnly cookies

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,15 +1,21 @@
-
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 import { catchError } from '../utils/catchError.js';
 
 export const protect = catchError(async (req, res, next) => {
-  let token = req.headers.authorization;
-  if (token && token.startsWith('Bearer')) {
-    const decoded = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id).select('-password');
-    return next();
+  let token = req.cookies.token; // Read token from httpOnly cookie
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select('-password');
+      return next();
+    } catch (error) {
+      res.status(401);
+      throw new Error('Not authorized, token failed');
+    }
   }
+
   res.status(401);
   throw new Error('Not authorized, token missing');
 });
