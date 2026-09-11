b3a40d4798c73b5c2ef8c96766b4305a4d531807
FREX007 <hailufirew0@gmail.com>
2026-09-11 18:28:02 +0300

    Add logout route to authRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,13 @@
 // routes/authRoutes.js
 import express from 'express';
 import multer from 'multer';
-import { registerUser, loginUser } from '../controllers/authController.js';
+import { registerUser, loginUser, logoutUser } from '../controllers/authController.js';
 
 const upload = multer({ storage: multer.memoryStorage() });
 const router = express.Router();
 
 router.post('/register', upload.single('businessLicenseFile'), registerUser);
 router.post('/login', loginUser);
+router.post('/logout', logoutUser);
 
 export default router;
