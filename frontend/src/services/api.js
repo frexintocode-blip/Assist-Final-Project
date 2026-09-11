20478863d23204be8199be70bb2d6b9ad2fa94e3
FREX007 <hailufirew0@gmail.com>
2026-09-11 18:10:35 +0300

    Update api.js with withCredentials and remove obsolete token interception

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,16 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
-});
-
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
+  withCredentials: true, // Required to send and receive httpOnly cookies automatically
 });
 
 export default API;
